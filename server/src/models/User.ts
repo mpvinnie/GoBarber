aee38b64729508380e16b8b1a18935b415026de3
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-10-07 08:22:48 -0400

    UserEntity

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -2,25 +2,29 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
-  CreateDateColumn
+  CreateDateColumn,
+  UpdateDateColumn
 } from 'typeorm'
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
   @PrimaryGeneratedColumn('uuid')
   id: string
 
   @Column()
-  provider: string
+  name: string
 
-  @Column('time with time zone')
-  date: Date
+  @Column()
+  email: string
+
+  @Column()
+  password: string
 
   @CreateDateColumn()
   created_at: Date
 
-  @CreateDateColumn()
+  @UpdateDateColumn()
   updated_at: Date
 }
 
-export default Appointment
+export default User
