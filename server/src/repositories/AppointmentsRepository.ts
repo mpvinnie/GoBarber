461727edb038e5aed12ae241074b71d3b40ed374
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-10-07 07:41:27 -0400

    Create and List Appointments

---
+++ server/src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,16 @@
+import Appointment from '../models/Appointment'
+
+import { EntityRepository, Repository } from 'typeorm'
+
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = await this.findOne({
+      where: { date }
+    })
+
+    return findAppointment
+  }
+}
+
+export default AppointmentsRepository
