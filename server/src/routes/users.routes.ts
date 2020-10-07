f8f893129abd47f4ad8da5e26a29ee61a57e0231
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-10-07 14:23:10 -0400

    Update User Avatar

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer'
 import uploadConfig from '../config/upload'
 
 import CreateUserService from '../services/CreateUserService'
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService'
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
@@ -27,8 +28,19 @@ usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  (request, response) => {
-    return response.json({ ok: true })
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService()
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename
+      })
+
+      return response.json(user)
+    } catch (err) {
+      return response.status(400).json({ error: err.message })
+    }
   }
 )
 
