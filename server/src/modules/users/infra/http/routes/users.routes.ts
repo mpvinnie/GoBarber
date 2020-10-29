4c09248c6800e1225438d2ad451843e0e7b98652
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-10-29 03:09:39 -0400

    refactoring users module

--- server/src/modules/users/infra/http/routes/users.routes.ts
+++ server/src/modules/users/infra/http/routes/users.routes.ts
@@ -6,14 +6,17 @@ import CreateUserService from '@modules/users/services/CreateUserService'
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService'
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
+import UsersRepository from '../../typeorm/repositories/UsersRepository'
 
 const usersRouter = Router()
 const upload = multer(uploadConfig)
 
 usersRouter.post('/', async (request, response) => {
+  const usersRepository = new UsersRepository()
+
   const { name, email, password } = request.body
 
-  const createUser = new CreateUserService()
+  const createUser = new CreateUserService(usersRepository)
 
   const user = await createUser.execute({ name, email, password })
 
@@ -25,7 +28,9 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const updateUserAvatar = new UpdateUserAvatarService()
+    const usersRepository = new UsersRepository()
+
+    const updateUserAvatar = new UpdateUserAvatarService(usersRepository)
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
--- server/src/modules/users/services/CreateUserService.ts
+++ server/src/modules/users/services/CreateUserService.ts
@@ -1,23 +1,22 @@
-import { getRepository } from 'typeorm'
 import { hash } from 'bcryptjs'
 
 import User from '@modules/users/infra/typeorm/entities/User'
 
 import AppError from '@shared/errors/AppError'
-
-interface IRequest {
-  name: string
-  email: string
-  password: string
-}
+import IUsersRepository from '../repositories/IUsersRepository'
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO'
 
 class CreateUserService {
-  public async execute({ name, email, password }: IRequest): Promise<User> {
-    const usersRepository = getRepository(User)
+  constructor(private usersRepository: IUsersRepository) {
+    /**/
+  }
 
-    const checkUserExists = await usersRepository.findOne({
-      where: { email }
-    })
+  public async execute({
+    name,
+    email,
+    password
+  }: ICreateUserDTO): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email)
 
     if (checkUserExists) {
       throw new AppError('Email address already used')
@@ -25,13 +24,11 @@ class CreateUserService {
 
     const hashedPassword = await hash(password, 8)
 
-    const user = usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password: hashedPassword
     })
-
-    await usersRepository.save(user)
 
     return user
   }
