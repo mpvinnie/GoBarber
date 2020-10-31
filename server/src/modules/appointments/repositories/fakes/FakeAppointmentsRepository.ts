8548d612d61f4e32ba5843bbb816c8ac228a530f
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-10-31 06:12:50 -0400

    List months available

--- server/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ server/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,8 +1,9 @@
 import { v4 as uuid } from 'uuid'
-import { isEqual } from 'date-fns'
+import { isEqual, getMonth, getYear } from 'date-fns'
 
 import IAppointmentsRepository from '../IAppointmentsRepository'
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO'
+import IFindAllInMonthProviderDTO from '@modules/appointments/dtos/IFindAllInMonthProviderDTO'
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment'
 
@@ -10,11 +11,26 @@ class FakeAppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = []
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find((appointment) =>
+    const findAppointment = this.appointments.find(appointment =>
       isEqual(appointment.date, date)
     )
 
     return findAppointment
+  }
+
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year
+  }: IFindAllInMonthProviderDTO): Promise<Appointment[]> {
+    const appointments = this.appointments.filter(
+      appointment =>
+        appointment.provider_id === provider_id &&
+        getMonth(appointment.date) + 1 === month &&
+        getYear(appointment.date) === year
+    )
+
+    return appointments
   }
 
   public async create({
