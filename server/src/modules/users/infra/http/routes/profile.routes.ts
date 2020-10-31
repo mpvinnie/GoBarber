00142f50ec8fd312622bc8d2cc52e62c9495744d
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-10-31 14:52:40 -0400

    validating routes with celebrate

--- server/src/modules/users/infra/http/routes/profile.routes.ts
+++ server/src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 import ProfileController from '../controllers/ProfileController'
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
@@ -8,7 +9,19 @@ const profieController = new ProfileController()
 
 profileRouter.use(ensureAuthenticated)
 
-profileRouter.put('/', profieController.update)
 profileRouter.get('/', profieController.show)
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password'))
+    }
+  }),
+  profieController.update
+)
 
 export default profileRouter
