00271d047f6824d19a06becf7b2defa5de566487
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-01 08:37:44 -0400

    config s3 storage provider

--- server/src/shared/container/providers/StorageProvider/index.ts
+++ server/src/shared/container/providers/StorageProvider/index.ts
@@ -1,9 +1,12 @@
 import { container } from 'tsyringe'
+
 import DiskStorageProvider from './implementations/DiskStorageProvider'
 import IStorageProvider from './models/IStorageProvider'
+import S3StorageProvider from './implementations/S3StorageProvider'
 
 const providers = {
-  disk: DiskStorageProvider
+  disk: DiskStorageProvider,
+  s3: S3StorageProvider
 }
 
 container.registerSingleton<IStorageProvider>('StorageProvider', providers.disk)
