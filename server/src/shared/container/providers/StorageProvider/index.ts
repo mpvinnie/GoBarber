9fcec6a75003be4eac2f67070d733d3df1074b85
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-01 08:53:54 -0400

    config uploads

--- server/src/shared/container/providers/StorageProvider/index.ts
+++ server/src/shared/container/providers/StorageProvider/index.ts
@@ -1,7 +1,9 @@
 import { container } from 'tsyringe'
+import uploadConfig from '@config/upload'
+
+import IStorageProvider from './models/IStorageProvider'
 
 import DiskStorageProvider from './implementations/DiskStorageProvider'
-import IStorageProvider from './models/IStorageProvider'
 import S3StorageProvider from './implementations/S3StorageProvider'
 
 const providers = {
@@ -9,4 +11,7 @@ const providers = {
   s3: S3StorageProvider
 }
 
-container.registerSingleton<IStorageProvider>('StorageProvider', providers.disk)
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  providers[uploadConfig.driver]
+)
