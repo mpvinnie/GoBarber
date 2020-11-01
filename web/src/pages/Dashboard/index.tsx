dc8e7742a9ba57125b59e3acb41e2ce2ef93e6bc
Vinicius Peres <vinniemalafaia@hotmail.com>
2020-11-01 16:36:22 -0400

    header dashboard

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import { FiPower } from 'react-icons/fi'
 
-import { Container } from './styles'
+import logoImg from '../../assets/logo.svg'
+import { useAuth } from '../../hooks/auth'
+
+import { Container, Header, HeaderContent, Profile } from './styles'
 
 const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth()
+
   return (
     <Container>
-      <h1>Dashboard</h1>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="Go Barber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>Vinicius Peres</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
     </Container>
   )
 }
