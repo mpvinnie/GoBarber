7346e4c4b54b40f1b2681b584bc357a8874558a6
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-01-23 15:51:29 -0400

    header list barbers

--- appgobarber/src/pages/CreateAppointment/index.tsx
+++ appgobarber/src/pages/CreateAppointment/index.tsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import Icon from 'react-native-vector-icons/Feather'
+import { useAuth } from '../../hooks/auth'
 
-import { Text } from 'react-native'
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar
+} from './styles'
 
-import { Container } from './styles'
+interface IRouteParams {
+  providerId: string
+}
 
 const CreateAppointment: React.FC = () => {
+  const { user } = useAuth()
+  const route = useRoute()
+  const { goBack } = useNavigation()
+
+  const { providerId } = route.params as IRouteParams
+
+  const navigateBack = useCallback(() => {
+    goBack()
+  }, [goBack])
+
   return (
     <Container>
-      <Text>CreateAppointment</Text>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#959991" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleileiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
     </Container>
   )
 }
