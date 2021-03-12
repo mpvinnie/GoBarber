25acfaa8ab89386740dd0ca646afcf569c49c650
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-03-11 23:50:16 -0400

    authenticate routes

---
+++ server/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '8e1d3b34fd7683c621315c3f7222a718',
+    expiresIn: '1d'
+  }
+}
--- server/src/services/AuthenticateUserService.ts
+++ server/src/services/AuthenticateUserService.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm'
 import { compare } from 'bcryptjs'
 import { sign } from 'jsonwebtoken'
 
+import authConfig from '../config/auth'
+
 import User from '../models/User'
 
 interface IRequest {
@@ -30,9 +32,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination')
     }
 
-    const token = sign({}, '8e1d3b34fd7683c621315c3f7222a718', {
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d'
+      expiresIn
     })
 
     return { user, token }
