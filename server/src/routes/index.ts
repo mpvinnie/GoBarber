8e984920d30ae254c64f2f28ffbafb3de933b9a9
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-03-12 15:22:32 -0400

    dealing with errors

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 
 import appointmentsRouter from './appointments.routes'
-import usersRouter from './users.routes'
 import sessionsRouter from './sessions.routes'
+import usersRouter from './users.routes'
 
 const routes = Router()
 
