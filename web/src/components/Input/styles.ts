947c8bb9eb72f93a0251fdd139110a87363a7a46
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-03-14 02:39:50 -0400

    styling tooltip

--- web/src/components/Input/styles.ts
+++ web/src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components'
 
+import Tooltip from '../Tooltip'
+
 interface IContainerProps {
   isFocused: boolean
   isFilled: boolean
+  isErrored: boolean
 }
 
 export const Container = styled.div<IContainerProps>`
@@ -19,6 +22,12 @@ export const Container = styled.div<IContainerProps>`
   & + div {
     margin-top: 8px;
   }
+
+  ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
 
   ${(props) =>
     props.isFocused &&
@@ -45,5 +54,23 @@ export const Container = styled.div<IContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `
