69742d4310b33721a9f5f8ed56906da154f27a53
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-03-14 15:31:59 -0400

    animating toast messages

--- web/src/components/ToastContainer/Toast/index.tsx
+++ web/src/components/ToastContainer/Toast/index.tsx
@@ -7,6 +7,7 @@ import { Container } from './styles'
 
 interface IToastProps {
   message: IToastMessage
+  style: object
 }
 
 const icons = {
@@ -15,7 +16,7 @@ const icons = {
   success: <FiCheckCircle size={24} />
 }
 
-const Toast: React.FC<IToastProps> = ({ message }) => {
+const Toast: React.FC<IToastProps> = ({ message, style }) => {
   const { removeToast } = useToast()
 
   useEffect(() => {
@@ -29,7 +30,11 @@ const Toast: React.FC<IToastProps> = ({ message }) => {
   }, [message.id, removeToast])
 
   return (
-    <Container type={message.type} hasDescription={!!message.description}>
+    <Container
+      type={message.type}
+      hasDescription={!!message.description}
+      style={style}
+    >
       {icons[message.type || 'info']}
 
       <div>
