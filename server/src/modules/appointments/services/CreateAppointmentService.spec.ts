469442c0c60a717e8f419fb40f01efbec657ee18
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-03-16 15:28:20 -0400

    create first test

--- server/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ server/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,27 @@
-test('sum to numbers', () => {
-  expect(1 + 1).toBe(2)
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
+import CreateAppointmentService from './CreateAppointmentService'
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository
+let createAppointment: CreateAppointmentService
+
+describe('CreateAppointment', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository()
+
+    createAppointment = new CreateAppointmentService(fakeAppointmentsRepository)
+  })
+
+  it('should be able to create a new appointment', async () => {
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123'
+    })
+
+    expect(appointment).toHaveProperty('id')
+    expect(appointment.provider_id).toBe('123123')
+  })
+
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect(1 + 2).toBe(3)
+  // })
 })
