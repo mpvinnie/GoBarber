05d37a3a90957aff7fa91aba4f6282b23c32e0ff
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-03-17 00:15:49 -0400

    finish tests

---
+++ server/src/modules/users/infra/http/routes/password.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+
+import ForgotPasswordController from '../controllers/ForgotPasswordController'
+import ResetPasswordController from '../controllers/ResetPasswordController'
+
+const passwordRouter = Router()
+
+const forgotPassword = new ForgotPasswordController()
+const resetPassword = new ResetPasswordController()
+
+passwordRouter.post('/forgot', forgotPassword.create)
+passwordRouter.post('/reset', resetPassword.create)
+
+export default passwordRouter
