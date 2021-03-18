bcc532a0051fb3f6770bb9402d2584fa03ce87a6
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-03-18 06:24:32 -0400

    config upload with amazon s3

--- server/src/modules/users/infra/http/routes/users.routes.ts
+++ server/src/modules/users/infra/http/routes/users.routes.ts
@@ -12,7 +12,7 @@ import { celebrate, Joi, Segments } from 'celebrate'
 const usersRouter = Router()
 const usersController = new UsersController()
 const userAvatarController = new UsersAvatarController()
-const upload = multer(uploadConfig)
+const upload = multer(uploadConfig.multer)
 
 usersRouter.post(
   '/',
