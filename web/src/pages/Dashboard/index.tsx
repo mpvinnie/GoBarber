e17fd158092023a8f35fda09a222ef121b3486fa
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-03-19 21:05:27 -0400

    list appointments

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import { FiPower } from 'react-icons/fi'
+import React, { useState } from 'react'
+import { FiClock, FiPower } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg'
 
 import { useAuth } from '../../hooks/auth'
 
-import { Container, Header, HeaderContent, Profile } from './styles'
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+  Appointment,
+  Section
+} from './styles'
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date())
+
   const { user, signOut } = useAuth()
 
   return (
@@ -29,6 +42,88 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 19</span>
+            <span>Sexta-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="http://localhost:3333/files/88653bb51776f6a7e6c0-Eu.jpg"
+                alt="Vinicius Malafaia"
+              />
+
+              <strong>Vinicius Malafaia</strong>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                10:00
+              </span>
+
+              <div>
+                <img
+                  src="http://localhost:3333/files/88653bb51776f6a7e6c0-Eu.jpg"
+                  alt="Vinicius Malafaia"
+                />
+
+                <strong>Vinicius Malafaia</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                14:00
+              </span>
+
+              <div>
+                <img
+                  src="http://localhost:3333/files/88653bb51776f6a7e6c0-Eu.jpg"
+                  alt="Vinicius Malafaia"
+                />
+
+                <strong>Vinicius Malafaia</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                16:00
+              </span>
+
+              <div>
+                <img
+                  src="http://localhost:3333/files/88653bb51776f6a7e6c0-Eu.jpg"
+                  alt="Vinicius Malafaia"
+                />
+
+                <strong>Vinicius Malafaia</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   )
 }
