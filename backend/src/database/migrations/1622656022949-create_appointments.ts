cc3654034525911699a0cbe4322a037a225fee15
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-06-05 07:19:19 -0400

    fix appointments migration

--- backend/src/database/migrations/1622656022949-create_appointments.ts
+++ backend/src/database/migrations/1622656022949-create_appointments.ts
@@ -2,7 +2,6 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
 export class createAppointments1622656022949 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     await queryRunner.createTable(
       new Table({
         name: 'appointments',
@@ -10,9 +9,7 @@ export class createAppointments1622656022949 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
-            isPrimary: true,
-            generationStrategy: 'uuid',
-            default: 'uuid_generate_v4()'
+            isPrimary: true
           },
           {
             name: 'provider',
