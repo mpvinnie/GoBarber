e92ab374e723baa6ffa4a3edbeddaf23461448c3
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-06-06 09:52:17 -0400

    serving static files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,15 @@ import 'reflect-metadata'
 import express from 'express'
 
 import './database'
+
+import uploadConfig from './config/upload'
 import { appRoutes } from './routes'
 
 const app = express()
 
 app.use(express.json())
 
+app.use('/files', express.static(uploadConfig.directory))
 app.use(appRoutes)
 
 app.listen(3333, () => {
