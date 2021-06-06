c0968352a32485f2d3856a80c355436572d7d676
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-06-06 12:29:32 -0400

    add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata'
+import cors from 'cors'
 import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import './database'
@@ -9,8 +10,8 @@ import { appRoutes } from './routes'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
-
 app.use('/files', express.static(uploadConfig.directory))
 app.use(appRoutes)
 
